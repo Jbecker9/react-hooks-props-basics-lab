fa2470444afeae959c4daa2b612d8740aefe1c2d
Jared <jbeckersalax9@aol.com>
2022-01-28 14:54:00 -0500

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,12 @@ import React from "react";
 import Links from './Links';
 
 function About(props) {
-  // console.log(`Second here: ${props.github}`)
+  console.log(`Second here: ${props.bio}`)
 
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{props.bio}</p>
+      {props.bio && props.bio.length > 0 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links 
         linkedin = {props.linkedin}
